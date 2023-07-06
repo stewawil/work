65dfdf9bac16e19febcad4002087ddc22cd4508c
Wilson Stewart <Wilsonstewart13@gmail.com>
2023-07-06 13:49:25 -0700

    almost

---
+++ signageData.js
@@ -0,0 +1,45 @@
+var signageData = [
+  {
+    discountValue: "$4.50",
+    productName: "CORONA 12 BOTTLES",
+    priceValue: "$27",
+    validFrom: "July 3",
+    validTo: "July 30, 2023"
+  },
+  {
+    discountValue: "$2.00",
+    productName: "HEINEKEN 6-PACK",
+    priceValue: "$12.99",
+    validFrom: "July 1",
+    validTo: "July 31, 2023"
+  },
+  {
+    discountValue: "$2.00",
+    productName: "HEINEKEN 6-PACK",
+    priceValue: "$12.99",
+    validFrom: "July 1",
+    validTo: "July 31, 2023"
+  },
+  {
+    discountValue: "$2.00",
+    productName: "HEINEKEN 6-PACK",
+    priceValue: "$12.99",
+    validFrom: "July 1",
+    validTo: "July 31, 2023"
+  },
+  {
+    discountValue: "$2.00",
+    productName: "HEINEKEN 6-PACK",
+    priceValue: "$12.99",
+    validFrom: "July 1",
+    validTo: "July 31, 2023"
+  },
+  {
+    discountValue: "$2.00",
+    productName: "HEINEKEN 6-PACK",
+    priceValue: "$12.99",
+    validFrom: "July 1",
+    validTo: "July 31, 2023"
+  },
+  // Add more signage data objects here
+];
